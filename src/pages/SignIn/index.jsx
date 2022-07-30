501bcea9a7e71d2307d2bce7bdad7e6b64162f47
Renan Marvila <renan.marvilla@gmail.com>
2022-07-29 23:34:15 -0300

    name change

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -22,7 +22,7 @@ export function SignIn(){
   return(
     <Container>
       <Form>
-        <h1>Rocket Notes</h1>
+        <h1>MarvNotes</h1>
         <p>Aplicação para salvar e gerenciar links úteis</p>
         <h2>Faça seu login</h2>
         
